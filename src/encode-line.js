61ab16c35d05f35463e55b755f8ff7e2dfdec13c
Tatiana <tanik.sam@yandex.ru>
2022-04-30 23:04:59 +0300

    encose-line add

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function encodeLine(str ) {
+  let strArr=str.split('').reverse()
+  
+  let newStrArr=[];
+ 
+ 
+let j=1;
+  for (let i=0;i<strArr.length;i++){
+  if ((strArr[i])!==strArr[i+1]){
+ 
+    newStrArr.push(strArr[i])
+  
+    if(j!==1){newStrArr.push(j)}
+    j=1;}
+    else{j++;}
+
+  }
+  let newStr=newStrArr.reverse().join('');
+  
+  return(newStr)
 }
+
 
 module.exports = {
   encodeLine
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
- function transform(/*arr*/) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  let arrNew=[];
+  let i=0;
+  if (!Array.isArray(arr)){
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+  }
+  while (i<arr.length){
+if (typeof arr[i]==='number'){ 
+  
+  arrNew.push(arr[i])
+  i++;
 }
-
+else{
+   switch (arr[i]){
+   case '--discard-next':
+     console.log (arr[i],i)
+      i+=2;
+      if ((arr[i]==='--discard-prev')||(arr[i]==='--double-prev'))
+      {
+        i+=1;}
+      break;
+   case '--discard-prev':
+      console.log (arr[i],i)
+      arrNew.pop()
+      i++;
+      break;
+  case '--double-next':
+      arrNew.push(arr[i+1]);
+      i++;
+      break;
+  case '--double-prev':
+      arrNew.push(arr[i-1]);
+      i++;
+      break;
+      default: return(arrNew)
+}
+}
+  }
+return(arrNew)}
 module.exports = {
   transform
 };
